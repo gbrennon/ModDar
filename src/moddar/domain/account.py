d3dc318c45ac6daa9ce276889f6d67ae0253421e
Glauber Brennon <glauberbrennon@gmail.com>
2026-05-17 01:00:00 -0300

    feat(domain): impl Post and User domain models

--- src/moddar/domain/account.py
+++ src/moddar/domain/account.py
@@ -3,21 +3,21 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from moddar.domain.post import Post
+    from moddar.domain.link import Link
 
 
-class User:
-    """Represents a Reddit user who creates posts."""
+class Account:
+    """Represents a Reddit account (user)."""
 
     def __init__(
         self,
         id: str,
         username: str,
-        posts: list[Post] | None = None,
+        links: list[Link] | None = None,
     ) -> None:
         self._id = id
         self._username = username
-        self._posts: list[Post] = posts if posts is not None else []
+        self._links: list[Link] = links if links is not None else []
 
     @property
     def id(self) -> str:
@@ -28,20 +28,20 @@ class User:
         return self._username
 
     @property
-    def posts(self) -> list[Post]:
-        return self._posts
+    def links(self) -> list[Link]:
+        return self._links
 
-    def add_post(self, post: Post) -> None:
-        """Add a post to this user's list of posts."""
-        self._posts.append(post)
+    def add_link(self, link: Link) -> None:
+        """Add a Link to this account's list of links."""
+        self._links.append(link)
 
     def __eq__(self, other: object) -> bool:
-        if not isinstance(other, User):
+        if not isinstance(other, Account):
             return NotImplemented
         return self._id == other._id
 
     def __repr__(self) -> str:
         return (
-            f"User(id={self._id!r}, username={self._username!r}, "
-            f"posts={self._posts!r})"
+            f"Account(id={self._id!r}, username={self._username!r}, "
+            f"links={self._links!r})"
         )
